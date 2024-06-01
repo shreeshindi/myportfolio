f3cc6bb224f37ca7e60cc2cb954f73be12d9913f
shreenidhi <shreenidhi.mc.vernekar@gmail.com>
2024-06-01 22:49:38 +0530

    foother

--- funpart/Footer.tsx
+++ funpart/Footer.tsx
@@ -7,16 +7,16 @@ const Footer: FC = () => {
     <footer className="bg-gray-800 text-green-200 py-4 mt-8 w-full text-center">
       <div className="flex flex-col items-center space-y-4">
         <div className="flex justify-center space-x-12">
-          <a href="https://www.youtube.com/" target="_blank" rel="noopener noreferrer" className={`hover:text-white ${styles.link}`}>
+          <a href="https://www.youtube.com/channel/UCbpQHosJd4BhKeqyKqrezTw" target="_blank" rel="noopener noreferrer" className={`hover:text-white ${styles.link}`}>
             <span className={styles.typingEffect} data-text="YouTube">YouTube</span>
           </a>
-          <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer" className={`hover:text-white ${styles.link}`}>
+          <a href="https://www.instagram.com/shreenidhi.mc/" target="_blank" rel="noopener noreferrer" className={`hover:text-white ${styles.link}`}>
             <span className={styles.typingEffect} data-text="Instagram">Instagram</span>
           </a>
-          <a href="https://github.com/" target="_blank" rel="noopener noreferrer" className={`hover:text-white ${styles.link}`}>
+          <a href="https://github.com/shreeshindi" target="_blank" rel="noopener noreferrer" className={`hover:text-white ${styles.link}`}>
             <span className={styles.typingEffect} data-text="GitHub">GitHub</span>
           </a>
-          <a href="https://www.linkedin.com/" target="_blank" rel="noopener noreferrer" className={`hover:text-white ${styles.link}`}>
+          <a href="https://www.linkedin.com/in/shreenidhi-mc-vernekar-29a050259/" target="_blank" rel="noopener noreferrer" className={`hover:text-white ${styles.link}`}>
             <span className={styles.typingEffect} data-text="LinkedIn">LinkedIn</span>
           </a>
         </div>

e591971fef5f3789c46109dd33f7a7c048b03d77
shreenidhi <shreenidhi.mc.vernekar@gmail.com>
2024-06-01 22:43:55 +0530

    foother thing

--- funpart/Footer.tsx
+++ funpart/Footer.tsx
@@ -1,22 +1,28 @@
 // /funpart/Footer.tsx
 import { FC } from 'react';
+import styles from './Footer.module.css';
 
 const Footer: FC = () => {
   return (
     <footer className="bg-gray-800 text-green-200 py-4 mt-8 w-full text-center">
-      <div className="flex justify-center space-x-6">
-        <a href="https://www.youtube.com/" target="_blank" rel="noopener noreferrer" className="hover:text-white">
-          YouTube
-        </a>
-        <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer" className="hover:text-white">
-          Instagram
-        </a>
-        <a href="https://github.com/" target="_blank" rel="noopener noreferrer" className="hover:text-white">
-          GitHub
-        </a>
-        <a href="https://www.linkedin.com/" target="_blank" rel="noopener noreferrer" className="hover:text-white">
-          LinkedIn
-        </a>
+      <div className="flex flex-col items-center space-y-4">
+        <div className="flex justify-center space-x-12">
+          <a href="https://www.youtube.com/" target="_blank" rel="noopener noreferrer" className={`hover:text-white ${styles.link}`}>
+            <span className={styles.typingEffect} data-text="YouTube">YouTube</span>
+          </a>
+          <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer" className={`hover:text-white ${styles.link}`}>
+            <span className={styles.typingEffect} data-text="Instagram">Instagram</span>
+          </a>
+          <a href="https://github.com/" target="_blank" rel="noopener noreferrer" className={`hover:text-white ${styles.link}`}>
+            <span className={styles.typingEffect} data-text="GitHub">GitHub</span>
+          </a>
+          <a href="https://www.linkedin.com/" target="_blank" rel="noopener noreferrer" className={`hover:text-white ${styles.link}`}>
+            <span className={styles.typingEffect} data-text="LinkedIn">LinkedIn</span>
+          </a>
+        </div>
+        <div className="text-green-400">
+          &copy; 2024
+        </div>
       </div>
     </footer>
   );

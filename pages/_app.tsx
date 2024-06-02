f7c61b41b3d7573269feaf418141a648fb91cc89
shreenidhi <shreenidhi.mc.vernekar@gmail.com>
2024-06-02 16:55:55 +0530

    app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,7 @@
+// pages/_app.tsx
 import "@/styles/globals.css";
+import "@/styles/Home.module.css";
+import "@/styles/Eye.module.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {

3f0260ba2bd6c6f1873bbea37fc4ad473a4a2075
shreenidhi <shreenidhi.mc.vernekar@gmail.com>
2024-06-02 13:36:24 +0530

    deploy for master

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: '/myportfolio', // replace with your repository name
+  assetPrefix: '/myportfolio/', // replace with your repository name
 };
 
 export default nextConfig;
